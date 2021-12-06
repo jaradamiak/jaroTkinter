b5a293cd270f667d615e4e50f22c0de240a3426f
jaradamiak <jaradamiak@gmail.com>
2021-12-06 23:35:35 +0200

    database 2

--- database.py
+++ database.py
@@ -8,20 +8,91 @@ root.title("Windows")
 root.iconbitmap('images/unisa.ico')
 root.geometry("400x400")
 
-conn = sqlite3.connect('address_book.db')
+# c.execute("""CREATE TABLE addresses (
+#     first_name text,
+#     last_name text,
+#     address text,
+#     city text,
+#     state text,
+#     zipcode integer
+# )      """)
+def submit():
+    conn = sqlite3.connect('address_book.db')
+    c = conn.cursor()
+    c.execute("INSERT INTO addresses VALUES (:f_name, :l_name, :address, :city, :state, :zipcode)",
+              {
+                  'f_name':f_name.get(),
+                  'l_name':l_name.get(),
+                  'address':address.get(),
+                  'city':city.get(),
+                  'state':state.get(),
+                  'zipcode':zipcode.get()
+              })
+    
 
-c = conn.cursor()
-c.execute("""CREATE TABLE addresses (
-    first_name text,
-    last_name text,
-    address text,
-    city text,
-    state text,
-    zipcode integer
-)      
-          """)
-conn.commit()
+    conn.commit()
+    conn.close()
+    
+    f_name.delete(0, END)
+    l_name.delete(0, END)
+    address.delete(0, END)
+    city.delete(0, END)
+    state.delete(0, END)
+    zipcode.delete(0, END)
+    
 
-conn.close()
+def query():
+    conn = sqlite3.connect('address_book.db')
+    c = conn.cursor()
+    c.execute("SELECT *, oid FROM ADDRESSES")
+    records = c.fetchall()
+    print(records)
+    
+    print_records = ""
+    for record in records:
+        print_records += str(record[0]) + " " + str(record[1]) + str(record[6]) + "\n"
+    
+    query_label = Label(root, text=print_records)
+    query_label.grid(row=8, column=0, columnspan=2)
+    
+    conn.commit()
+    conn.close()
+    
+
+#Create text boxes
+f_name = Entry(root, width=30)
+f_name.grid(row=0, column=1, padx=20)
+l_name = Entry(root, width=30)
+l_name.grid(row=1, column=1, padx=20)
+address = Entry(root, width=30)
+address.grid(row=2, column=1, padx=20)
+city = Entry(root, width=30)
+city.grid(row=3, column=1, padx=20)
+state = Entry(root, width=30)
+state.grid(row=4, column=1, padx=20)
+zipcode = Entry(root, width=30)
+zipcode.grid(row=5, column=1, padx=20)
+
+f_name_label = Label(root, text="First Name")
+f_name_label.grid(row=0, column=0)
+l_name_label = Label(root, text="Last Name")
+l_name_label.grid(row=1, column=0)
+address_label = Label(root, text="Address")
+address_label.grid(row=2, column=0)
+city_label = Label(root, text="City")
+city_label.grid(row=3, column=0)
+state_label = Label(root, text="State")
+state_label.grid(row=4, column=0)
+zipcode_label = Label(root, text="Zipcode")
+zipcode_label.grid(row=5, column=0)
+
+submit_button = Button(root, text="Add record", command=submit)
+submit_button.grid(row=6, column=0, columnspan=2, pady=10, padx=10, ipadx=100)
+    
+query_btn = Button(root, text="Show records", command=query)
+query_btn.grid(row=7, column=0, columnspan=2, pady=10, padx=10, ipadx=137)
+
+quit_btn = Button(root, text="Quit", command=root.quit)
+quit_btn.grid(row=10, column=0, columnspan=2, pady=10, padx=10, ipadx=80)
 
 root.mainloop()
